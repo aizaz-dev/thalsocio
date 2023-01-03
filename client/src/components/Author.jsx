2193fd1a5f97b9a28a4559184f4ec3903dd033fb
Muhammad Abdur Rehman Wasti <45505896+syedwastil@users.noreply.github.com>
2023-01-03 08:20:23 +0500

    client create post and user widget done

---
+++ client/src/components/Author.jsx
@@ -0,0 +1,85 @@
+import React,{useState,useEffect} from 'react';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
+import UserImage from "./UserImage";
+
+const Author = ({ authorId }) => {
+  const [author, setAuthor] = useState(null)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.token);
+  const { palette } = useTheme();
+  const primaryLight = palette.primary.light;
+  const primaryDark = palette.primary.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  const getAuthor = async () => {
+    const response = await fetch(`http://localhost:3001/api/user/${authorId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setAuthor(data);
+  };
+
+  useEffect(() => {
+    getAuthor();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!author) {
+    return null;
+  }
+
+  const {
+    name,
+    pic,
+  } = author;
+ 
+
+  return (
+    <FlexBetween>
+      <FlexBetween gap="1rem">
+        <UserImage image={pic} size="55px" />
+        <Box
+          onClick={() => {
+            navigate(`/profile/${authorId}`);
+            navigate(0);
+          }}
+        >
+          <Typography
+            color={main}
+            variant="h5"
+            fontWeight="500"
+            sx={{
+              "&:hover": {
+                color: palette.primary.light,
+                cursor: "pointer",
+              },
+            }}
+          >
+            {name}
+          </Typography>
+        </Box>
+      </FlexBetween>
+      <IconButton>
+        <MoreVertOutlinedIcon/>
+      </IconButton>
+      {/* <IconButton  
+        onClick={() => patchFriend()}
+        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+      >
+        {isFriend ? (
+          <PersonRemoveOutlined sx={{ color: primaryDark }} />
+        ) : (
+          <PersonAddOutlined sx={{ color: primaryDark }} />
+        )}
+      </IconButton> */}
+    </FlexBetween>
+  );
+};
+
+export default Author;
--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,9 +1,88 @@
 import React from 'react'
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "../navBar/index";
+import UserWidget from "../widgets/UserWidget";
+import CreatePostWidget from '../widgets/CreatePostWidget';
+import Author from '../../components/Author';
+import PostWidget from '../widgets/PostWidget';
 
 function HomePage() {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id } = useSelector((state) => state.user);
   return (
-    <div>HomePage</div>
+    <Box>
+      <Navbar/>
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+      <Box flexBasis={isNonMobileScreens ? "15%" : undefined} >
+          
+      </Box>
+      <Box flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"} >
+          <CreatePostWidget/>
+          <PostWidget/>
+      </Box>
+      <Box flexBasis={isNonMobileScreens ? "30%" : undefined}>
+      <UserWidget userId={_id}  />
+      </Box>
+      </Box>
+    </Box>
   )
 }
 
+
 export default HomePage
+
+/*
+
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "scenes/navbar";
+
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+
+
+const HomePage = () => {
+
+
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default HomePage;
+*/
